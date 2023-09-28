d6ee8151fa3516ff321e3d6162b079a224dc45ba
CodingManoj <mvp2612@hotmail.com>
2023-09-27 22:26:28 -0400

    Git Auto Push

--- schema.tf
+++ schema.tf
@@ -1,11 +1,11 @@
 resource "null_resource" "schema" {
   provisioner "local-exec" {
-    command = <<EOF 
-cd /tmp 
-curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-unzip mysql.zip 
-cd mysql-main
-mysql -h ${aws_db_instance.mysql.address} -u admin1 -p roboshop1 < shipping.sql 
-EOF   
-  }
+         command = <<EOF
+            cd /tmp 
+            curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+            unzip -o /tmp/mysql.zip 
+            cd mysql-main
+            mysql -h ${aws_db_instance.mysql.address} -uadmin1 -pRoboShop1 < shipping.sql
+        EOF 
+    }
 }
