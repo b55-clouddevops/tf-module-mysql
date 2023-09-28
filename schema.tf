b52638612d015bae9af9d109603b836c678d9c77
CodingManoj <mvp2612@hotmail.com>
2023-09-27 22:22:17 -0400

    Git Auto Push

--- schema.tf
+++ schema.tf
@@ -4,7 +4,7 @@ resource "null_resource" "schema" {
     command = <<EOF 
 cd /tmp 
 curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-unzip -o mysql.zip 
+unzip mysql.zip 
 cd mysql-main 
 mysql -h ${aws_db_instance.address.mysql}   -uadmin1 -proboshop1 < shipping.sql 
 EOF 
