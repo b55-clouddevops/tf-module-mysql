f66fc92d55bff615513269a84cb7b459e99c14d7
CodingManoj <mvp2612@hotmail.com>
2023-09-27 22:19:30 -0400

    Git Auto Push

--- schema.tf
+++ schema.tf
@@ -6,7 +6,7 @@ cd /tmp
 curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
 unzip -o mysql.zip 
 cd mysql-main 
-mysql -h connectionString ?  -uadmin1 -proboshop1 < shipping.sql 
+mysql -h ${aws_db_instance.address.mysql}   -uadmin1 -proboshop1 < shipping.sql 
 EOF 
   
   }
