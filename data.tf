663c7f808ab4fc88c36bb192bb6b45d9d1ea5887
CodingManoj <mvp2612@hotmail.com>
2023-10-01 22:37:27 -0400

    Git Auto Push

--- data.tf
+++ data.tf
@@ -8,4 +8,12 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
+# fetches the information of the secret
+data "aws_secretsmanager_secret" "secrets" {
+  name = "robot/secrets"
+}
 
+# Fetches the secrets version from the above server
+data "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
